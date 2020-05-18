d13936342b59e9de9d091106033be7d19a5c230d
Walter Hoerman <whoerman@gmail.com>
2020-05-17 20:35:40 -0400

    adding the map and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react';
+import USAMap from "react-usa-map";
+import Header from "./layouts/Header";
+import './App.css';
 
 function App() {
+
+  const mapHandler = (event) => {
+    console.log('You clicked on ' + event.target.dataset.name);
+  };
+
+  const statesCustomConfig = () => {
+    return {
+      "NJ": {
+        fill: "navy",
+      },
+      "NY": {
+        fill: "#CC0000"
+      },
+      "MN": {
+        fill: "#CC0000"
+      }
+    };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="App-header">
+        <div className="d-none d-flex-md justify-content-center">
+        <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
+        </div>
+      
+      </div>
     </div>
   );
 }
---
+++ client/src/layouts/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+  return (
+    <div className = "HomeDiv">
+      <h1 className="d-flex justify-center">Appleseed Initiatve Electric Grid Visualization</h1>
+    </div>
+  );
+}
+
+export default Header;
