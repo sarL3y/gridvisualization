ce56a1bd49421eabd972c79df877a060b52b34f0
Walter Hoerman <whoerman@gmail.com>
2020-05-18 09:12:10 -0400

    random colors and seperate state array

--- client/src/layouts/Header.js
+++ client/src/layouts/Header.js
@@ -5,7 +5,6 @@ function Header() {
   return (
     <div className = "HeaderDiv">
       <div className="row">
-      <img src="../client/public/Appleseedlogo.jpg" alt="Appleseed Logo"/>
       <h1 className="maintitle">Appleseed Initiative Electric Grid Visualization</h1>
       </div>
     </div>
--- client/src/layouts/StateCustomConfig.js
+++ client/src/layouts/StateCustomConfig.js
@@ -1,41 +1,172 @@
-let stateArray = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA",
-  'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN',
-  'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK',
-  'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VT', 'WA', 'WI', 'WV',
-  'WY'
-];
+import stateArray from "./StateArray";
+
+const colorArray = ["#6C4385", "#FFE047", "#016C0B", "#2210D3", "#DD4B3E", "#822F2B", ];
+
+let stateColorObj = {};
+// let currentStateColor = "";
 
 function statesCustomConfig() {
 
-  let goodState = "green";
-  let badState = "brown";
-  let hotState = "red";
-  let coolState = "blue"
+  // let stateColorEach = stateArray.map(
+  //   let currentStateColor = colorArray[(Math.floor(Math.random() * 6))];
+  //   console.log(currentStateColor)
+  // )
 
-  return {
-    "HI": {
-      fill: goodState,
+  stateColorObj = {
+    "AK": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
-    "NJ": {
-      fill: hotState,
+    "AL": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
-    "NY": {
-      fill: coolState,
+    "AR": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
-    "MN": {
-      fill: hotState,
+    "AZ": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    "CA": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    "CO": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    "CT": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    "DE": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
     "FL": {
-      fill: badState,
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
-    "UT": {
-      fill: goodState,
+    "GA": {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     },
-    "TX": {
-      fill: badState
+    'HI': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'IA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'ID': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'IL': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'IN': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'KS': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'KY': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'LA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MD': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'ME': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MI': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MN': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MO': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MS': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'MT': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NC': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'ND': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NE': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NH': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NJ': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NM': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NV': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'NY': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'OH': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'OK': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'OR': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'PA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'RI': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'SC': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'SD': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'TN': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'TX': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'UT': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'VA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'VT': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'WA': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'WI': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'WV': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
+    },
+    'WY': {
+      fill: colorArray[(Math.floor(Math.random() * 6))],
     }
+
   };
+
+  return stateColorObj
 };
 
 export default statesCustomConfig;
-
