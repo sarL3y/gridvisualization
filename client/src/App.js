af22b13a314c8cd7c6cb0078e595564db90b6f5d
Walter Hoerman <whoerman@gmail.com>
2020-05-18 20:59:56 -0400

    Adding buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import USAMap from "react-usa-map";
 import Header from "./layouts/Header";
 import Footer from "./layouts/Footer";
+import Wind from "./layouts/Wind";
+import Button from 'react-bootstrap/Button';
 import statesCustomConfig from "./layouts/StateCustomConfig";
 import './App.css';
 
@@ -16,7 +18,19 @@ function App() {
       <Header className="header" />
       <div className="MapDiv">
         <div className="d-none d-flex-md justify-content-center">
-          <h4>Click on a State to see their energy profile</h4>
+          <h4> Click on a State to see their energy profile</h4>
+          <div className = "ButtonDiv">
+        <div className="row">
+            <Button variant="primary">>>>></Button>{'  Click here for Top Ten: '}
+            <Button variant="secondary" onClick={Wind}>Wind</Button>{' '}
+            <Button variant="danger" onClick={Wind}>Solar</Button>{' '}
+            <Button variant="primary" onClick={Wind}>Hydro</Button>{' '}
+            <Button variant="primary" onClick={Wind}>Nuclear</Button>{' '}
+            <Button variant="primary" onClick={Wind}>Petroleum</Button>{' '}
+            <Button variant="primary" onClick={Wind}>Coal</Button>{' '}
+            <Button variant="primary" onClick={Wind}>Other Biomass</Button>{' '}
+        </div>
+    </div>
         <USAMap customize={statesCustomConfig()} onClick={mapHandler} />
         </div>
         <Footer />
---
+++ client/src/layouts/Wind.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+function Wind() {
+  return {
+};
+  return Wind;
+};
+
+export default Wind;
